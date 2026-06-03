a40a0844833a1276ea9f2f6241fbe832716ad2b8
luisazpuruag-ux <luis.azpuruag@gmail.com>
2026-06-02 23:53:35 -0400

    first commit

---
+++ calculadorabasica.py
@@ -0,0 +1,17 @@
+# calculadora_basica.py
+
+print("CALCULADORA BÁSICA")
+
+numero1 = float(input("Ingrese el primer número: "))
+numero2 = float(input("Ingrese el segundo número: "))
+
+suma = numero1 + numero2
+resta = numero1 - numero2
+multiplicacion = numero1 * numero2
+division = numero1 / numero2
+
+print("\nResultados:")
+print(f"Suma: {suma}" )
+print("Resta:", resta)
+print("Multiplicación:", multiplicacion)
+print("División:", division)
